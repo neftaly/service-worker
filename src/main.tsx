bbf1efae999e3c993445c74feddf5849c3292ad7
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2025-09-13 21:22:56 +1200

    Rename register-sw

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 
 import "./index.css";
-import "./registerSW";
+import "./register-sw.ts";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("Failed to find root.");
