126263b0f103c879ce0cff80cae78445e9a39554
Neftaly Hernandez <neftaly.hernandez@gmail.com>
2025-09-13 20:25:54 +1200

    Add import map

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { serwist } from "@serwist/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 const serwistConfig = {
   swSrc: "src/sw.ts",
@@ -8,10 +9,26 @@ const serwistConfig = {
   injectionPoint: "self.__SW_MANIFEST",
 };
 
+const importMap = {
+  imports: {
+    serwist: "https://esm.sh/serwist@9.2.1",
+    "@serwist/core": "https://esm.sh/@serwist/core@9.2.1",
+    "@serwist/vite/worker": "https://esm.sh/@serwist/vite@9.2.1/worker",
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
+    createHtmlPlugin({
+      minify: false,
+      inject: {
+        data: {
+          importMap: JSON.stringify(importMap, null, 2),
+        },
+      },
+    }),
     serwist({
       ...serwistConfig,
       type: "classic",
@@ -23,6 +40,9 @@ export default defineConfig({
       type: "module",
       swDest: "sw-module.js",
       rollupFormat: "es",
+      rollupOptions: {
+        external: Object.keys(importMap.imports),
+      },
     }),
   ],
 });
